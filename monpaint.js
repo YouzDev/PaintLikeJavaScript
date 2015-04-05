6d20b718ce2a2df628bcbd3fcfe3ef2fdfbe2883
zitoun_a <zitoun_a@epitech.eu>
2015-04-06 01:42:18 +0200

    rendu by zitoun_a on pc-zitoun_a @ lun. avril  6 01:42:18 CEST 2015

--- monpaint.js
+++ monpaint.js
@@ -1,188 +1,254 @@
-$(document).ready(function() {
-	
-	var color = "#000"; // couleur de base du crayon
-	var painting = false;
-	var started = false;	
-	// var width_brush = 5;
-	var canvas = $("#myCanvas"); // sur quoi dessiner
-	var cursorX, cursorY;
-	// var restoreCanvasArray = [];
-	// var restoreCanvasIndex = 0;
-	
-	var context = canvas[0].getContext('2d');
-	
-	// // Trait arrondi :
-	// context.lineJoin = 'round'; // créer un coin arrondi quand 2 lignes se croisent
-	// context.lineCap = 'round'; // créer une ligne avec bout arrondi
+/*jslint browser: true node:true*/
+/*global $, jQuery, alert*/
+"use strict";
+$(document).ready(function () {
+    $("#myCanvas").hide("slow", function () {
 
 
-	// Click souris enfoncé sur le canvas, je dessine :
-	canvas.mousedown(function(e) {
-		painting = true;
-		
-		// CoordonnÃ©es de la souris :
-		// cursorX = (e.pageX - this.offsetLeft);
-		// cursorY = (e.pageY - this.offsetTop);
+        var color = "#000", painting = false, started = false, canvas = $("#myCanvas"), cursorX, cursorY, context = canvas[0].getContext('2d'), cursorX2, cursorY2, cursorX3, cursorY3;
+// var width_brush = 5;
+// Trait arrondi :
+// context.lineJoin = 'round'; // créer un coin arrondi quand 2 lignes se croisent
+// context.lineCap = 'round'; // créer une ligne avec bout arrondi
+// $('#crayon').click(function() {
+// Click souris enfoncé sur le canvas, je dessine :
+        canvas.mousedown(function (e) {
+            painting = true;
+// CoordonnÃ©es de la souris :
+            cursorX = (e.pageX - this.offsetLeft);
+            cursorY = (e.pageY - this.offsetTop);
+        });
+// Relachement du Click sur tout le document, j'arrete de dessiner :
+        $(this).mouseup(function (e) {
+            painting = false;
+            started = false;
+            e.preventDefault();
+        });
+// Mouvement de la souris sur le canvas :
+        canvas.mousemove(function (e) {
+// Si je suis en train de dessiner (click souris enfonce) :
+            if (painting) {
+// Set Coordonnees de la souris :
+                cursorX = (e.pageX - this.offsetLeft) - 10; // 10 = decalage du curseur
+                cursorY = (e.pageY - this.offsetTop) - 10;
 
-	});
-	
-	// Relachement du Click sur tout le document, j'arrete de dessiner :
-	$(this).mouseup(function(e) {
-		painting = false;
-		started = false;
+ drawLine();
+            }
+        });
+        $("#montre").click(function () {
+            $("#myCanvas").fadeTo("slow", 0.9);
 
-		// cursorX2 = (e.pageX - this.offsetLeft);
-		// cursorY2 = (e.pageY - this.offsetTop);
-	});
-	
-	// Mouvement de la souris sur le canvas :
-	canvas.mousemove(function(e) {
-		// Si je suis en train de dessiner (click souris enfonce) :
-		if (painting) {
-			// Set Coordonnees de la souris :
-			cursorX = (e.pageX - this.offsetLeft) - 10; // 10 = decalage du curseur
-			cursorY = (e.pageY - this.offsetTop) - 10;
-			
-			// Dessine une ligne :
-			 drawLine();
-		}
+        });
+// Fonction qui dessine une ligne :
+        function drawLine(){
+        // Si c'est le debut, j'initialise
+            if (!started) {
+            // Je place mon curseur pour la premiere fois :
+                context.beginPath();
+                context.moveTo(cursorX, cursorY);
+                started = true;
+            } else {
+                context.lineTo(cursorX, cursorY);
+                context.strokeStyle = document.getElementById('couleur').value;;
+            // context.lineWidth = width_brush;
+                context.stroke();
+            }
+        }
 
-	});
-	
-	// Fonction qui dessine une ligne :
-	function drawLine() {
-		// Si c'est le debut, j'initialise
-		if (!started) {
-			// Je place mon curseur pour la premiere fois :
-			context.beginPath();
-			context.moveTo(cursorX, cursorY);
-			started = true;
-		} 
-		// Sinon je dessine
-		else {
-			context.lineTo(cursorX, cursorY);
-			context.strokeStyle = color;
-			// context.lineWidth = width_brush;
-			context.stroke();
-		}
-	}
-	$('#trait').click(function() {
-		var lepremierclic = true;
-		canvas.click(function(e) {
-			if(lepremierclic == true) {
-				painting = false;
-				cursorX = (e.pageX - this.offsetLeft);
-				cursorY = (e.pageY - this.offsetTop);
-				context.beginPath();
-				context.moveTo(cursorX, cursorY);
+        $('#trait').click(function () {
+            canvas.unbind();
+            var lepremierclic = true;
+            canvas.click(function (e) {
+                if (lepremierclic === true) {
+                    painting = false;
+                    cursorX = (e.pageX - this.offsetLeft);
+                    cursorY = (e.pageY - this.offsetTop);
+                    context.beginPath();
+                    context.moveTo(cursorX, cursorY);
+                    lepremierclic = false;
+                } else {
+                    lepremierclic = true;
+                    cursorX2 = (e.pageX - this.offsetLeft);
+                    cursorY2 = (e.pageY - this.offsetTop);
+                    context.lineTo(cursorX2, cursorY2);
+                    context.strokeStyle = document.getElementById('couleur').value;
+                    context.stroke();
+                    context.closePath();
 
-				lepremierclic = false;
-			}
-			else {
-				lepremierclic = true;
-				cursorX2 = (e.pageX - this.offsetLeft);
-				cursorY2 = (e.pageY - this.offsetTop);
-				context.lineTo(cursorX2, cursorY2);
-				context.colorStrike = 'black';
-				context.stroke();
-				context.closePath();
+                }
 
-			}
+            });
 
-		});			
+        });
 
-	});
+        $('#rectangle').click(function () {
+            canvas.unbind();
+            var lepremierclic = true;
+            canvas.click(function (e) {
+                if (lepremierclic === true) {
+                    cursorX = (e.pageX - this.offsetLeft);
+                    cursorY = (e.pageY - this.offsetTop);
+                    context.beginPath();
+                    lepremierclic = false;
+                } else {
+                    lepremierclic = true;
+                    cursorX2 = (e.pageX - this.offsetLeft);
+                    cursorY2 = (e.pageY - this.offsetTop);
+            // console.log(cursorX2);
+                    cursorX3 = cursorX2 - cursorX;
+                    cursorY3 = cursorY2 - cursorY;
 
-	$('#rectangle').click(function() { 
-		var lepremierclic = true;
-		canvas.click(function(e) {
-		if(lepremierclic == true){
-				cursorX = (e.pageX - this.offsetLeft);
-				cursorY = (e.pageY - this.offsetTop);
-				context.beginPath();
-	console.log(cursorX);
-					lepremierclic = false;
-	}		
-		else {
-			lepremierclic = true;
-			cursorX2 = (e.pageX - this.offsetLeft);
-			cursorY2 = (e.pageY - this.offsetTop);
-			console.log(cursorX);
-			// console.log(cursorX2);
-			
+            // console.log(cursorX3);
 
-			cursorX3 = cursorX2 - cursorX;
-			cursorY3 = cursorY2 - cursorY;
+                    context.rect(cursorX, cursorY, cursorX3, cursorY3);
+                    context.strokeStyle = document.getElementById('couleur').value;
+                    context.stroke();
 
-			// console.log(cursorX3);
+                }
+            });
 
-			context.rect(cursorX, cursorY, cursorX3, cursorY3);
-			context.stroke();
+        });
 
-		}
-	});
+        $('#rectanglerempli').click(function () {
+            canvas.unbind();
+            var lepremierclic = true;
+            canvas.click(function (e) {
+                if (lepremierclic === true) {
+                    cursorX = (e.pageX - this.offsetLeft);
+                    cursorY = (e.pageY - this.offsetTop);
+                    context.beginPath();
+                    lepremierclic = false;
+                } else {
+                    lepremierclic = true;
+                    cursorX2 = (e.pageX - this.offsetLeft);
+                    cursorY2 = (e.pageY - this.offsetTop);
+            // console.log(cursorX2);
+                    cursorX3 = cursorX2 - cursorX;
+                    cursorY3 = cursorY2 - cursorY;
+
+            // console.log(cursorX3);
+
+                    context.rect(cursorX, cursorY, cursorX3, cursorY3);
+                    context.fillStyle = document.getElementById('couleur').value;
+                    context.fill();
+                    context.stroke();
+
+                }
+            });
+
+        });
+
+        $('#cercle').click(function () {
+            canvas.unbind();
+            var lepremierclic = true;
+            canvas.click(function (e) {
+                if (lepremierclic === true) {
+                    cursorX = (e.pageX - this.offsetLeft);
+                    cursorY = (e.pageY - this.offsetTop);
+                    context.beginPath();
+                    lepremierclic = false;
+                } else {
+                    lepremierclic = true;
+                    cursorX2 = (e.pageX - this.offsetLeft);
+                    cursorY2 = (e.pageY - this.offsetTop);
+
+                var truc = Math.sqrt((cursorX2 - cursorX) * (cursorX2 - cursorX) + (cursorY2 - cursorY) * (cursorY2 - cursorY));
+
+                context.arc(cursorX, cursorY, truc, 0, 2 * Math.PI);
+                context.strokeStyle = document.getElementById('couleur').value;
+                context.stroke();
+                }
+
+        });
+    });
+
+    $('#cerclerempli').click(function () {
+        canvas.unbind(); 
+        var lepremierclic = true;
+        canvas.click(function (e) {
+            if (lepremierclic === true) {
+                cursorX = (e.pageX - this.offsetLeft);
+                cursorY = (e.pageY - this.offsetTop);
+                context.beginPath();
+                lepremierclic = false;
+                } else {
+                   lepremierclic = true;
+                   cursorX2 = (e.pageX - this.offsetLeft);
+                   cursorY2 = (e.pageY - this.offsetTop);
+
+                   var truc = Math.sqrt((cursorX2 - cursorX) * (cursorX2 - cursorX) + (cursorY2 - cursorY) * (cursorY2 - cursorY));
+
+                   context.arc(cursorX, cursorY, truc, 0, 2 * Math.PI);
+                   context.fillStyle = document.getElementById('couleur').value;
+                   context.fill();
+                   context.stroke();
+            }
+
+        });
+    });
 
 
 
-	});
+    $('#lol').click(function () {
+        canvas.unbind(); 
+        canvas.mousedown(function (e) {
 
-	$('#lol').click(function() {
+            painting = false;
+            cursorX = (e.pageX - this.offsetLeft);
+            cursorY = (e.pageY - this.offsetTop);
+            context.beginPath();
+            context.moveTo(cursorX, cursorY);
 
-		canvas.mousedown(function(e) {
+            canvas.click(function() {
+                cursorX2 = (e.pageX - this.offsetLeft);
+                cursorY2 = (e.pageY - this.offsetTop);
+                context.lineTo(cursorX2, cursorY2);
+                context.strokeStyle = document.getElementById('couleur').value;
+                context.stroke();
+                context.closePath();
 
-			painting = false;
-			cursorX = (e.pageX - this.offsetLeft);
-			cursorY = (e.pageY - this.offsetTop);
-			context.beginPath();
-			context.moveTo(cursorX, cursorY);
+            });
 
-			canvas.click(function() {
-				cursorX2 = (e.pageX - this.offsetLeft);
-				cursorY2 = (e.pageY - this.offsetTop);
-				context.lineTo(cursorX2, cursorY2);
-				context.colorStrike = 'black';
-				context.stroke();
-				context.closePath();
+        });         
 
-			});
-
-		});			
-
-	});
+    });
 
 
-	// Clear du Canvas :
-	function clear_canvas() {
-		context.clearRect(0,0, canvas.width(), canvas.height());
-	}
-	
-	
-	$("#couleurs a").each(function() {
-		// Je lui attribut une couleur de fond :
-		$(this).css("background", $(this).attr("data-couleur"));
+    // Clear du Canvas :
+    function clear_canvas() {
+        context.clearRect(0,0, canvas.width(), canvas.height());
+    }
 
-		// Et au click :
-		$(this).click(function() {
-			// Je change la couleur du pinceau :
-			color = $(this).attr("data-couleur");
+    // $('#reset').click(function() {
 
-			// Et les classes CSS :
-			$("#couleurs a").removeAttr("class", "");
-			$(this).attr("class", "actif");
+    //  location.reload();
 
-			return false;
-		});
-	});
+    // });
+    
+    //  // Je lui attribut une couleur de fond :
+    //  $(this).css("background", $(this).attr("data-couleur"));
 
-	
-	// Bouton Reset :
-	$("#reset").click(function() {
-		// Clear canvas :
-		clear_canvas();
-		
-		
-	});
-	
+    //  // Et au click :
+    //  $(this).click(function() {
+    //      // Je change la couleur du pinceau :
+    //      color = $(this).attr("data-couleur");
 
+    //      // Et les classes CSS :
+    //      $("#couleurs a").removeAttr("class", "");
+    //      $(this).attr("class", "actif");
+
+    //      return false;
+    //  });
+    // });
+
+
+    // Bouton Reset :
+    $("#reset").click(function () {
+        // Clear canvas :
+        clear_canvas();
+        // location.reload();
+    });
+
+});
 
 });
